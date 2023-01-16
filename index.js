111aaaa48cb51174d79153d92b5da3c147eb7660
faro911 <fathur.r1112@gmail.com>
2023-01-16 19:11:51 +0700

    mencoba var let const dan tugas1.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var msg1 ="Hallo world";
+let msg2 ="Halo bang";
+const msg3 ="iya bank";
+
+if(true){
+    var msg1 ="1";
+    let msg2 ="2";
+
+    console.log("testing", msg2)
+
+}
+
+
+console.log(msg1)
+console.log(msg2)
+console.log(msg3)
---
+++ tugas1.js
@@ -0,0 +1,5 @@
+var nm1="Patur";
+let nm2="Rohman";
+const um1=20;
+
+console.log("hasilnya :", nm1, nm2 ,um1)
