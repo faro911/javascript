68c3cf0bc91da1a46f15c65108f3398f3c147b84
faro911 <fathur.r1112@gmail.com>
2023-01-16 21:39:00 +0700

    belajar return

---
+++ return.js
@@ -0,0 +1,19 @@
+function take(){
+    return "item di ambil";
+}
+
+function hold(){
+    var item = "didapat kan";
+    return item;
+}
+
+function ambil(){
+    var terambil ="item di dapat";
+    return terambil;
+}
+var jupuk = ambil();
+
+// result
+console.log(take());
+console.log(hold());
+console.log(jupuk);
