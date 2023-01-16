45b859d333a6724743177a53e587044ec3afe779
faro911 <fathur.r1112@gmail.com>
2023-01-16 21:14:29 +0700

    membuat function dan jenis penulisanya

---
+++ function.js
@@ -0,0 +1,17 @@
+function coffesatu() {
+    console.log("Coffe Arabica")
+}
+
+const coffedua = function() {
+    console.log("Coffe Late")
+}
+
+// dua diatass catatan function versi lama, yang bawah terbaru
+
+const coffetiga = ()=> {
+    console.log("Coffe Mocacino")
+}
+
+coffesatu();
+coffedua();
+coffetiga();
