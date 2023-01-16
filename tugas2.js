2fd72c029c3b8529965a4364576d62362a88a93b
faro911 <fathur.r1112@gmail.com>
2023-01-16 20:26:08 +0700

    membuat tugas2.js

---
+++ tugas2.js
@@ -0,0 +1,21 @@
+var nama="Pororo";
+var umur=12;
+var dewasa=false;
+var skill=null;
+var admds=[3,1,1,2]
+
+if(umur>15)
+dewasa=true;
+else
+dewasa=false;
+
+if(admds[0]>2)
+skill='attacker';
+else
+skill='defender';
+
+
+console.log("Hero", nama, "usia",umur,"tahun")
+console.log("Apakah Hero dewasa? :", dewasa)
+console.log("Tipe Hero :", skill)
+console.log("All stat", admds,".Detail : Atk", admds[0],",Def",admds[1],",Spd",admds[2],",MDef",admds[3])
