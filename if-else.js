67ee256b4627aa60029938c6c8390e0ac50bc903
faro911 <fathur.r1112@gmail.com>
2023-01-16 23:07:26 +0700

    latihan if-else.js

---
+++ if-else.js
@@ -0,0 +1,18 @@
+function element(){
+    var ele = 4;
+
+    if(ele===1){
+        console.log("Element World")
+    }
+    else if(ele===2){
+        console.log("Element Godkind")
+    }
+    else if(ele===3){
+        console.log("Element Devil")
+    }
+    else {
+        console.log("Element Neutral")
+    }
+}
+
+element()
