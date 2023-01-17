2b16d253c6f151874b534106f05a8cd729e29ab3
faro911 <fathur.r1112@gmail.com>
2023-01-17 12:57:47 +0700

    belajar inifinity pake for dan while

---
+++ infinity.js
@@ -0,0 +1,14 @@
+// pertama pakai for
+
+// for (i=0;i>=0;i++){
+// console.log("repeat ke", i)
+// }
+
+// kedua pakai while
+
+var i = 0
+
+while(true){
+    console.log("ulang ke", i)
+    i++;
+}
