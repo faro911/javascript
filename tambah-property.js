b311665aa88dd404b08459ada38356ab27e046af
faro911 <fathur.r1112@gmail.com>
2023-01-17 22:04:00 +0700

    tugas8js telah selesai dibuat

---
+++ tambah-property.js
@@ -0,0 +1,18 @@
+function dataitem () {
+    var item1 = {
+        type : 'Senjata',
+        Nama : 'Golok merah hitam',
+        unik : 'Extra ele dark',
+        price : 5000,
+        adsmd : [43,2,10,1,2],
+    }
+    item1.price = 4000000;
+    item1.tempa = 1;
+        // menambah tempa pada item1
+
+
+    console.log(item1)
+    console.log("Harga equip : GOLD",item1.price)
+}
+
+dataitem()
---
+++ tugas8.js
@@ -0,0 +1,16 @@
+function myskill(){
+    var resume = {
+        id : 1921,
+        placemenet : 'front end development',
+        total_project : 36,
+        skill_html : 70,
+        skill_css : 45,
+        skill_js : 60,
+    }
+ resume.skill_js = 90;
+
+ console.log(resume)
+ console.log(resume.skill_html)
+}
+
+myskill()
