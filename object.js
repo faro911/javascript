80b53678f3d4c9d1ced9abe20d90146e593fcb9a
faro911 <fathur.r1112@gmail.com>
2023-01-17 21:06:36 +0700

    modifikasi object untuk belajar array di dlm object

--- object.js
+++ object.js
@@ -4,6 +4,7 @@ function dataitem () {
         Nama : 'Golok merah hitam',
         unik : 'Extra ele dark',
         price : 5000,
+        adsmd : [43,2,10,1,2],
     }
 
     var item2 = {
@@ -11,6 +12,7 @@ function dataitem () {
         Nama : 'Dragoon Raja Iblis',
         unik : 'Anti absolut death',
         price : 10,
+        adsmd : [1,57,12,30,90],
     }
 
     console.log(item1)
@@ -24,6 +26,14 @@ function dataitem () {
     console.log(item2.Nama)
     console.log(item2.unik)
     console.log(item2.price)
+
+    // array-dalam obejck
+    console.log("")
+    console.log(item1.adsmd)
+    console.log(item1.adsmd[0])
+    console.log(item2.adsmd)
+    console.log(item2.adsmd[1])
+
 }
 
 dataitem()
