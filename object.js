c16296b128388bb3583d4d3a0f15ef473d06d156
faro911 <fathur.r1112@gmail.com>
2023-01-17 20:55:58 +0700

    belajar object

---
+++ object.js
@@ -0,0 +1,29 @@
+function dataitem () {
+    var item1 = {
+        type : 'Senjata',
+        Nama : 'Golok merah hitam',
+        unik : 'Extra ele dark',
+        price : 5000,
+    }
+
+    var item2 = {
+        type : 'Armor',
+        Nama : 'Dragoon Raja Iblis',
+        unik : 'Anti absolut death',
+        price : 10,
+    }
+
+    console.log(item1)
+    console.log(item1.type)
+    console.log(item1.Nama)
+    console.log(item1.unik)
+    console.log(item1.price)
+    console.log("")
+    console.log(item2)
+    console.log(item2.type)
+    console.log(item2.Nama)
+    console.log(item2.unik)
+    console.log(item2.price)
+}
+
+dataitem()
