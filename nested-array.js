cb618e3ec9a2cb1f67bd86c9496cd4b04f6ccb67
faro911 <fathur.r1112@gmail.com>
2023-01-17 15:11:35 +0700

    belajar nasted array

---
+++ nested-array.js
@@ -0,0 +1,15 @@
+function ambildata() {
+    var namakota = [["Jakarta","Bandung", "Depok"],
+                    ["Semarang", "Solo", "Yogyakarta"],
+                    ["Surabaya", "Malang", "Blitar"]]
+
+    console.log(namakota[0][1], namakota[1][0], namakota[2][2])
+}
+
+
+// tanda di namakota pada console adalah index, cnth index 0 pertama mrjuk array prtma, dan 1 index pda array pertama tersebut yaitu jakarta
+
+ambildata()
+
+
+
--- tugas6.js
+++ tugas6.js
@@ -1,15 +1,70 @@
 for (var i=0 ; i<=20 ; i++){
    
-function hasil(){
-    if(i=i+2){
-        console.log("Genap")
-    }
-    else {
-        console.log("Ganjil")
-    }
-}
+// function hasil(){
+//     if(i===1){
+//         console.log("ganjil ke", i)
+//     }
+//     else if(i===2){
+//         console.log("genap ke", i)
+//     }
+//     else if(i===3){
+//         console.log("ganjil ke", i)
+//     }
+//     else if(i===4){
+//         console.log("genap ke", i)
+//     }
+//     else if(i===5){
+//         console.log("ganjil ke", i)
+//     }
+//     else if(i===6){
+//         console.log("genap ke", i)
+//     }
+//     else if(i===7){
+//         console.log("ganjil ke", i)
+//     }
+//     else if(i===8){
+//         console.log("genap ke", i)
+//     }
+//     else if(i===9){
+//         console.log("ganjil ke", i)
+//     }
+//     else if(i===10){
+//         console.log("genap ke", i)
+//     }
+//     else if(i===11){
+//         console.log("ganjil ke", i)
+//     }
+//     else if(i===12){
+//         console.log("genap ke", i)
+//     }
+//     else if(i===13){
+//         console.log("ganjil ke", i)
+//     }
+//     else if(i===14){
+//         console.log("genap ke", i)
+//     }
+//     else if(i===15){
+//         console.log("ganjil ke", i)
+//     }else if(i===16){
+//         console.log("genap ke", i)
+//     }
+//     else if(i===17){
+//         console.log("ganjil ke", i)
+//     }else if(i===18){
+//         console.log("genap ke", i)
+//     }
+//     else if(i===19){
+//         console.log("ganjil ke", i)
+//     }
+//     else if(i===20){
+//         console.log("genap ke", i)
+//     }
+//     else {
+//         console.log("nol", i)
+//     }
+// }
 
-    console.log(hasil())
+hasil()
 }
 
 
