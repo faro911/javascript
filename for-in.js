1a1248db6f0fc82450a756d1885cffb9cb90b88c
faro911 <fathur.r1112@gmail.com>
2023-01-17 22:58:48 +0700

    belajar for in untuk pemanggilan data

---
+++ for-in.js
@@ -0,0 +1,13 @@
+function mahas(){
+    var siswa = {
+        nama : 'Bejo',
+        nim : 14062,
+        prodi: 'Sistem Informasi'
+    }
+
+ for (var x in siswa){
+    console.log(siswa[x])
+ }
+}
+
+mahas()
