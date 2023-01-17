3464055599bb4425d501bfeebe54b2d2a3f19142
faro911 <fathur.r1112@gmail.com>
2023-01-17 13:52:14 +0700

    tugas6 susah belom selesai

---
+++ tugas6.js
@@ -0,0 +1,15 @@
+for (var i=0 ; i<=20 ; i++){
+   
+function hasil(){
+    if(i=i+2){
+        console.log("Genap")
+    }
+    else {
+        console.log("Ganjil")
+    }
+}
+
+    console.log(hasil())
+}
+
+
