1f04aed09f11510c6d7227abf7d61db1aa2346b0
faro911 <fathur.r1112@gmail.com>
2023-01-17 11:00:43 +0700

    pengerjaan tugas4.js

---
+++ tugas4.js
@@ -0,0 +1,31 @@
+function data() {
+
+    var siswa1 = 172;
+    var siswa2 = 173;
+    var siswa3 = 160;
+ 
+    if(siswa1>siswa2){
+        if(siswa1>siswa3){
+            if(siswa2>siswa3){
+                console.log("Siswa1 tertinggi, urutan tertinggi:siswa1,siswa2,siswa3")
+            }
+            else{
+                console.log("Siswa1 tertinggi, urutan tertinggi:siswa1,siswa3,siswa2")
+            }
+        }
+        else{
+            console.log("Siswa3 tertiggi, urutan tertinggi:siswa3,siswa1,siswa2")
+        }
+    }
+    else if(siswa1>siswa3) {
+        console.log("siswa2 tertinggi, urutan tertinggi:siswa2,siswa1,siswa3")
+    }
+    else if(siswa2>siswa3) {
+        console.log("siswa2 tertinggi, urutan tertinggi:siswa2,siswa3,siswa1")
+    }
+    else{
+        console.log("siswa3 tertinggi, urutan tertinggi:siswa3,siswa2,siswa1")
+    }
+}
+
+data()
