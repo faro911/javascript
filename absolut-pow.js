c5dea4d44f0c3d1dccf16cddb7211e012737e209
faro911 <fathur.r1112@gmail.com>
2023-01-18 22:49:20 +0700

    rename file abs ke abs-pow, materi pow tentang perpangkatan

---
+++ absolut-pow.js
@@ -0,0 +1,10 @@
+var a =20;
+var b =-30;
+var pangkat =3;
+
+console.log(a,b)
+console.log(a+b)
+console.log(Math.abs(a+b)) //menampilakn nilai absolut, ketika hasil negatif akan dirubah ke positif
+console.log("=== atas materi abs === bawah materi pow ===")
+console.log(a,"pangkat",pangkat,"adalah ", Math.pow(a,pangkat)); 
+//menampilkan perpangkatan, a nilai var dan var pangkat adalah pangkatnya.
--- absolut.js
+++
@@ -1,6 +0,0 @@
-var a =20;
-var b =-30;
-
-console.log(a,b)
-console.log(a+b)
-console.log(Math.abs(a+b)) //menampilakn nilai absolut, ketika hasil negatif akan dirubah ke positif
