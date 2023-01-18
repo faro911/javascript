433ef820ffe0d665eea85ec422912f043bc96bb7
faro911 <fathur.r1112@gmail.com>
2023-01-18 19:48:46 +0700

    rename file array-splice-unshift ,unshift perintah utk mnmbhkn data diawal array

--- array-splice-unshfit.js
+++ array-splice-unshfit.js
@@ -6,6 +6,8 @@ function pilih(){
     var elemen2=elemen.splice(5,0,'Dark Eather')
     // splice bertujuan untuk menyisipkan, detail angka 1 berarti index ke 1, angka 0 berarti data setelahny tdk ada yg terhapus, jika di isi 1 maka data setelah penyisipan akan terhapus, bgt juga dengan 2 dan berikut2 ny
     var elemen2=elemen.splice(0,1) //splice dikiri bertujuan untuk pnghpusan data index ke 0, dan yang terhapus adalah data tsb, jika 1 dignti dua, maka data ke satu  yaitu index 0 dan data ke dua index satu yang akan terhapus, bgt juga seterusny.
+
+    var elemen2=elemen.unshift('Thunder','Lightning') //unshift bertujuan untuk menambahkan data di awal array
     return elemen
 }
 
