f5ccd2eda1b36c81c6b6671341c0b3581555a862
faro911 <fathur.r1112@gmail.com>
2023-01-18 10:53:47 +0700

    bealjar operator pnjumlhn, pengu, prkalian dll

---
+++ operator.js
@@ -0,0 +1,12 @@
+function mat(){
+    var a = 10;
+    var b = 2;
+// penulisan pertama
+// console.log(a+b)
+// penulisan kedua
+return a-b;
+}
+
+// mat()
+// tulisan kedua pake yg bwah
+console.log(mat())
