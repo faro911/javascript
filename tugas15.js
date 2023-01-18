8e7792c921d11357585e8a7b4a4796c28bee94d9
faro911 <fathur.r1112@gmail.com>
2023-01-18 23:32:09 +0700

    membuat tugas 15 dan 16 dan lancar

---
+++ tugas15.js
@@ -0,0 +1,12 @@
+function Segsiku(){
+    var ab = 8;
+    var bc = 6;
+    var ac = null;
+
+    ac = (Math.pow(ab,2))+(Math.pow(bc,2))
+    console.log("Nilai ab adalah", ab)
+    console.log("Nilai bc adalah", bc)
+    console.log("Panjang ac pada segitiga siku-siku tersebut adalah" ,ac)
+}
+
+Segsiku()
