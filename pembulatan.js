736c66a1c94f7bb0c503ee33cca80010c6b9e1f1
faro911 <fathur.r1112@gmail.com>
2023-01-18 21:51:58 +0700

    belajar pembulatan

---
+++ pembulatan.js
@@ -0,0 +1,4 @@
+var a = 12.7;
+console.log(Math.ceil(a)) //pembulatan ke atas
+console.log(Math.floor(a)) //pembulatan ke bawah
+console.log(Math.round(a)) // pembulatan antara lebih dr 0.5 ke atas, 0.4 ke bawah
