9a9e6f8cadd3b7ba0f7eb0426288ff67bc481e1b
faro911 <fathur.r1112@gmail.com>
2023-01-18 23:19:08 +0700

    rename file absolut-pow-sqrt-log jadi satu biar simple dan mempelajari minmax untuk pncrian nilai tertinggi dan terendah

--- absolut-pow-sqrt-log.js
+++ absolut-pow-sqrt-log.js
@@ -1,10 +1,20 @@
-var a =20;
+var a =25;
 var b =-30;
 var pangkat =3;
 
 console.log(a,b)
 console.log(a+b)
 console.log(Math.abs(a+b)) //menampilakn nilai absolut, ketika hasil negatif akan dirubah ke positif
+
 console.log("=== atas materi abs === bawah materi pow ===")
 console.log(a,"pangkat",pangkat,"adalah ", Math.pow(a,pangkat)); 
 //menampilkan perpangkatan, a nilai var dan var pangkat adalah pangkatnya.
+
+console.log("===Bawah materi SQRT===")
+console.log(Math.sqrt(a)); //materi sqrt yaitu akar dari bilangan a.
+
+console.log("===Bwah materi log===")
+console.log(Math.log(a)) //ini artinya log nilai var a
+console.log(Math.log2(a)) // ini artinya 2 log var a
+console.log(Math.log10(a))// ini artinya 10 log var a
+
---
+++ minmax.js
@@ -0,0 +1,8 @@
+function minmax(){
+    var min = Math.min(10,3,100,2) //menampilkan nilai terkecil
+    console.log(min)
+    var max = Math.max(10,3,100,2) //menampilkan nilai terbesar
+    console.log(max)
+}
+
+minmax()
