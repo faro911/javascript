881b7e4df159cb14639802dd9c1a677be8f5727d
faro911 <fathur.r1112@gmail.com>
2023-01-18 13:20:55 +0700

    beljar logika operator

---
+++ operator-perbandingan.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b = 12;
+
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(a==b)
+console.log(a===b)
+console.log(a!=b)
+console.log(a!==b)
---
+++ oprat-logika.js
@@ -0,0 +1,5 @@
+var x =10;
+
+console.log(x=10 && x>5)
+console.log(x<9 || x > 20)
+console.log(!false)
