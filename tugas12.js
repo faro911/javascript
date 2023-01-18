e73380c1d0c73514ae9c358646cf5a1dbd7b3e44
faro911 <fathur.r1112@gmail.com>
2023-01-18 18:21:40 +0700

    tugas 12 selesai

---
+++ tugas12.js
@@ -0,0 +1,10 @@
+function buah(){
+    var kelbuah = ['pisang','jeruk','apel','mangga']
+    console.log(kelbuah)
+    var kelbuah2=kelbuah.pop()
+    console.log(kelbuah)
+    var kelbuah2=kelbuah.shift()
+    return kelbuah
+
+}
+console.log(buah())
