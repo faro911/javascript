7016af3f42ae66579db3d387cea3d362cc867e33
faro911 <fathur.r1112@gmail.com>
2023-01-18 21:58:55 +0700

    mengerjakan tugas14 dengan aman

---
+++ tugas14.js
@@ -0,0 +1,8 @@
+var a = 37;
+var tgl =31;
+var pembagian = a/tgl;
+
+console.log(pembagian)
+console.log(Math.ceil(pembagian))
+console.log(Math.floor(pembagian))
+console.log(Math.round(pembagian))
