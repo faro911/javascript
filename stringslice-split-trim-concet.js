be27e26ef8b212af0be26858a6a60a11b36a4dfb
faro911 <fathur.r1112@gmail.com>
2023-01-20 00:01:23 +0700

    lanjut pmbljaran concet tentang penambahan data, dan perubahan file stingslice untuk 4 materi ini,liat contekan untuk deetail

--- stringslice-split-trim-concet.js
+++ stringslice-split-trim-concet.js
@@ -1,5 +1,5 @@
 function potongan(){
-    var judul = " samehadaku ";
+    var judul = " samehadaku";
 
     console.log("===Materi bawah string slace===")
 
@@ -16,6 +16,15 @@ function potongan(){
 
     console.log(judul)
     console.log(judul.trim()) // fungsi trim untuk memotong wide spasi diawal dan akhir var
+
+    console.log("===Materi bawah tentang concet===")
+
+    var keterangan = "tempat";
+    var keterangan2= keterangan.concat(" Nonton"); //menambahkan data string
+
+    console.log(keterangan2)
+    console.log(judul,keterangan2.concat(" Film", " Anime")) //menmbhkan data sring dengan bbrp string
+    
 
 }
 potongan()
