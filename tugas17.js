48d19e4b8e3cb2b920cd877c9848a4d421387a4f
faro911 <fathur.r1112@gmail.com>
2023-01-19 16:22:07 +0700

    kerjakan tugas 17 tntang lowerupercase dan pncarian kode aski

---
+++ tugas17.js
@@ -0,0 +1,18 @@
+function tugas17(){
+    var a = "Saya beLajar bahaSa peMrograman deNgan khUsuk"
+    var b = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual."
+
+    var c ="Lorem Ipsum"
+
+    console.log(a.toLowerCase())
+    console.log(a.toUpperCase())
+    console.log("===")
+    console.log(b.toLowerCase())
+    console.log(b.toUpperCase())
+    console.log("=== di atas upper dan lower, di bawah pencari kode aski")
+    console.log(c.codePointAt(0),c.codePointAt(1),c.codePointAt(2),c.codePointAt(3),c.codePointAt(4),c.codePointAt(5))
+
+}
+
+
+tugas17()
