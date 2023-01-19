ee8d51c3ad5a7623fcc8c89a94d2ea01013b1c33
faro911 <fathur.r1112@gmail.com>
2023-01-19 15:08:12 +0700

    belajar upercase dan lowercase

---
+++ upercaselowercase.js
@@ -0,0 +1,7 @@
+function rubahhuruf(){
+    var kata = "Aku SuKa KamU";
+
+    console.log(kata.toLowerCase()) // untuk merubah variabel kata menjadi huruf kecil semua
+    console.log(kata.toUpperCase()) // untuk merubah variabel kata menjadi huruf besar semua
+}
+rubahhuruf()
