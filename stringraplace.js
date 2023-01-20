af64ba72e822477917ac0ddf56597f06e1b823d1
faro911 <fathur.r1112@gmail.com>
2023-01-20 18:33:54 +0700

    belajar tentang fungsi replace untuk mengganti data string dengan data yang lain

---
+++ stringraplace.js
@@ -0,0 +1,7 @@
+function isi() {
+    let cari = "Ini isi dari data program";
+    console.log(cari);
+    console.log(cari.replace("program", "penjualan")); //berfungsi mengganti program dengan penjualan
+}
+
+isi()
