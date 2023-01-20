528b5f1d8277ff945aed9350d74618c93129e319
faro911 <fathur.r1112@gmail.com>
2023-01-20 09:25:26 +0700

    mengerjakan tugas 19 tentang repeat

---
+++ tugas19.js
@@ -0,0 +1,19 @@
+function perulangan(){
+var a = "Semangat....";
+var b = "*";
+
+console.log(a.repeat(5)); //tugas ke 1
+
+// tugas ke 2
+console.log(b.repeat(5));
+console.log(b.repeat(20));
+console.log(b.repeat(30));
+console.log(b.repeat(40));
+console.log(b.repeat(55));
+console.log(b.repeat(40));
+console.log(b.repeat(30));
+console.log(b.repeat(20));
+console.log(b.repeat(5));
+
+}
+perulangan()
