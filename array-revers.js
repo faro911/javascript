c7e816f910181131d72a495dd5e152f5f35f3f7b
faro911 <fathur.r1112@gmail.com>
2023-01-20 18:49:39 +0700

    belajar aray revers untuk memabalikan data pada array

---
+++ array-revers.js
@@ -0,0 +1,7 @@
+function orra(){
+    let kota = ["Jakarta", "Surabaya", "Semarang", "Yogyakarta"];
+    console.log(kota);
+    return kota.reverse() //fungsi membalikn data pada array
+
+}
+console.log(orra())
