e16cfd63d5db2a5154e3d46e5ac341832e6c0a58
faro911 <fathur.r1112@gmail.com>
2023-01-21 16:11:14 +0700

    mengerjakan tugas 21 tntng mereplace data di dlm varabel string dengen data lain

---
+++ array-slice.js
@@ -0,0 +1,7 @@
+function ptongan(){
+    let isi = ["a","b","c","d","e","f","g"];
+    let cop = isi.slice(3, 6); //mengkopy fungsi arry ke dalam variabel yang kita buat sehingga hasilnya akan asam, keterangan : 3 di atas dimksdu dr index 3 dan seterusny akan di panggil data array nya, sedangkan 6 dimsksd index setelah 6 tidak di tampilkan
+    console.log(cop)
+}
+
+ptongan()
---
+++ tugas21.js
@@ -0,0 +1,8 @@
+function gntipre(){
+    let kata = "Saya belajar masak nasi goreng";
+
+    console.log(kata);
+    console.log(kata.replace("masak", "makan"));
+}
+
+gntipre()
