5933fa23a8889918c80b854e2c00b9d2bb018b29
faro911 <fathur.r1112@gmail.com>
2023-01-22 23:21:14 +0700

    belajar fungsi find di array untuk mencari nilai pertama yang keluar dalam pencarian nilai lebih dr 10, maka hasil adalah 12. materi findindex ga jelas abang2 niomic

---
+++ array-find-findindex.js
@@ -0,0 +1,11 @@
+let arre1=[5,12,8,120,44];
+
+let cari = arre1.find(function(element){
+    return element >10;
+})
+
+console.log(cari);
+
+//nb atas: fungsi find untuk mencari data aray pertama yang memenuhi kriteria dalam pencarian, contoh di atas didalam data array, kita mencari nilai yang lebih besar dari 10, sehingga hasilnya data pertama yng keluar adalah 12.
+
+//materi findindex ga jelas video 56 materi javascript
