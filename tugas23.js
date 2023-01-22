7d9282399bb23e5be800aa40baabda2411b2c1d1
faro911 <fathur.r1112@gmail.com>
2023-01-22 22:08:23 +0700

    mengerjakan tugas 23 tentang memfilter pencarian data di dalam array yang nilanya lbih besar dari 15.

---
+++ tugas23.js
@@ -0,0 +1,9 @@
+ let angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+ let pncarian = angka.filter(angkanya =>{
+    return angkanya > 15;
+ });
+
+ console.log(pncarian);
+
+ //tugas 23 tentang pencarian data array yang nilai nya lebih besar dari 15,
