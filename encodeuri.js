0340fe91dea2ca054cc90a61d76f1d425bedd641
faro911 <fathur.r1112@gmail.com>
2023-01-23 18:03:44 +0700

    belajar tentang materi encodeURI

---
+++ encodeuri.js
@@ -0,0 +1,4 @@
+let kalim = "my area.php?/noe.hub.index 99";
+let urei = encodeURI(kalim);
+
+console.log(urei); //fungsi encodeURI untuk menampilkai nilai unik kode selain ?/$% dan diganti dengan kode untik tersebut, contoh sepasi diganti dengan %20.
