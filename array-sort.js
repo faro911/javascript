286c6e9c219f35459b40295dc459391e60723ef5
faro911 <fathur.r1112@gmail.com>
2023-01-23 10:16:06 +0700

    belajar materi sort dan mengerjakan tugas24 yitu menggunakan fungsi sort

---
+++ array-sort.js
@@ -0,0 +1,11 @@
+function pnggil(){
+    let kota = ['jakarta','bandung','semarang','yogyakarta','surabaya'];
+
+    console.log(kota);
+    console.log('=========================================');
+    kota =kota.sort(); //mengurutkan berdasarkan abjad 
+
+    return kota
+}
+
+console.log(pnggil());
---
+++ tugas24.js
@@ -0,0 +1,11 @@
+function cale(){
+    let awaln = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+    
+    console.log(awaln);
+    awalacd = awaln.sort();
+    console.log(awalacd);
+    console.log(awalacd.reverse());
+}
+
+cale()
+
