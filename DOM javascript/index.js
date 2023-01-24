bab3750123cf4e2d42378a7512a11bfe6174c9a8
faro911 <fathur.r1112@gmail.com>
2023-01-24 13:55:50 +0700

    mengerjakan tugas32 dan belajar mencari id dijavascript menggunakan document.hetElementById()

--- DOM javascript/index.js
+++ DOM javascript/index.js
@@ -1,3 +1,12 @@
 function panggilalert(){
     alert("Sukses !")
 }
+
+function jalankanscrpt1(){
+    console.log("Sukses !")
+}
+
+function panggilid(){
+    console.log(document.getElementById("namadepan"));
+    console.log(document.getElementById("namabelakang"));
+}
