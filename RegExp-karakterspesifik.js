d6997f4a334d37dc4dfa0497fe62898608fd9d8b
faro911 <fathur.r1112@gmail.com>
2023-01-24 09:10:07 +0700

    memperbaruhi materi regexp-karakterspesifik

--- RegExp-karakterspesifik.js
+++ RegExp-karakterspesifik.js
@@ -1,13 +1,27 @@
 function takeword(value){
-    let ambilData = value.match(/Belajar Niomic/g); //mengambil data by matching
+    let ambilData = value.match(/Belajar Niomic/g); //mengambil data by matching meghasilkan data sesuai tipe data
     console.log(ambilData);
 }
 
 takeword("Saya Belajar Niomic");
 
 function takewords(value){
-    let ambilData2 = value.match(/[ai]/g ); //mengambil data by huruf
+    let ambilData2 = value.match(/[ai]/g ); //mengambil data by huruf [a,i], menghasilkan array
     console.log(ambilData2);
 }
 
 takewords("Saya Belajar Niomic");
+
+function takewordse(value){
+    let ambilData3 = value.match(/[^ai]/g ); //mengambil data by huruf terkecuali a dan i menghasilkan array
+    console.log(ambilData3);
+}
+
+takewordse("Saya Belajar Niomic");
+
+function takewordses(value){
+    let ambilData4 = value.match(/[a-i]/g ); //mengambil karakter antara a-i menghasilkan array (tidak termasuk hruf kapital)
+    console.log(ambilData4);
+}
+
+takewordses("Saya Belajar Niomic");
